3adee3645778cd09697d377f7468d1a34bb5f105
Teamese Felix <teamesef@gmail.com>
2019-02-17 21:30:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(holiday, days) {
-  return "It's 21 days until Independence Day!"
+  return "It's 20 days until Mother's Day!"
 }
